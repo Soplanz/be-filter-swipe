d81f3a02e733bbe43ac1e63cb50c065257ea5cf0
soplanz <70798003+Soplanz@users.noreply.github.com>
2023-05-21 12:42:56 +0700

    Add files via upload

---
+++ consumer.py
@@ -0,0 +1,18 @@
+import pika
+import json
+
+import requests
+import websockets
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+channel = connection.channel()
+channel.queue_declare(queue='notifications')
+
+def callback(ch, method, properties, body):
+    data = json.loads(body)
+    print({data['message']})
+
+channel.basic_consume(queue='notifications', on_message_callback=callback, auto_ack=True)
+
+print(' [*] Waiting for messages. To exit press CTRL+C')
+channel.start_consuming()
